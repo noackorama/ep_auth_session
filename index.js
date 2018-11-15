f7010c469606ad6b557027f882630f596af75df4
Marcus <git@luniki.de>
2018-11-15 09:41:23 +0100

    use the same whitespace everywhere (spaces)

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 exports.registerRoute = function(hook_name, args, cb) {
-	args.app.get("/auth_session", function(req, res) {
-		var r = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' + "\n";
+  args.app.get("/auth_session", function(req, res) {
+    var r =
+      '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' +
+      "\n";
 
-		r += '<html>' + "\n";
-		r += '<head>' + "\n";
-		r += '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">' + "\n";
-		r += '</head>' + "\n";
-		r += '<body>' + "\n";
-		r += '<script type="text/javascript">' + "\n";
+    r += "<html>" + "\n";
+    r += "<head>" + "\n";
+    r +=
+      '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">' +
+      "\n";
+    r += "</head>" + "\n";
+    r += "<body>" + "\n";
+    r += '<script type="text/javascript">' + "\n";
 
-		if (req.query.sessionID) {
-			r += 'document.cookie = "sessionID=' + encodeURIComponent(req.query.sessionID) + '; path=/;";' + "\n";
-		}
+    if (req.query.sessionID) {
+      r +=
+        'document.cookie = "sessionID=' +
+        encodeURIComponent(req.query.sessionID) +
+        '; path=/;";' +
+        "\n";
+    }
 
-		if (req.query.padName) {
-			var redirectUrl = '/p/';
+    if (req.query.padName) {
+      var redirectUrl = "/p/";
 
-			if (req.query.groupID) {
-				redirectUrl += encodeURIComponent(req.query.groupID) + '$';
-			}
+      if (req.query.groupID) {
+        redirectUrl += encodeURIComponent(req.query.groupID) + "$";
+      }
 
-			redirectUrl += encodeURIComponent(req.query.padName);
-			r += 'document.location.href="' + redirectUrl + '";' + "\n";
-		}
+      redirectUrl += encodeURIComponent(req.query.padName);
+      r += 'document.location.href="' + redirectUrl + '";' + "\n";
+    }
 
-		r += '</script>' + "\n";
-		r += '</body>' + "\n";
-		r += '</html>' + "\n";
+    r += "</script>" + "\n";
+    r += "</body>" + "\n";
+    r += "</html>" + "\n";
 
-		res.send(r);
-	});
+    res.send(r);
+  });
 };
